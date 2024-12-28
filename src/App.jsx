b4384796ee675687e939ad01d49ec6ba3c2e4cc6
SANJAY <153006829+sanjay271106@users.noreply.github.com>
2024-12-28 14:27:00 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { lazy } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+
+
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+           
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
